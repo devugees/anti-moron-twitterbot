96001f97147bf5d92975ada1bb990a35a95ea8d1
ronitronitronit <ronitronitronit@gmail.com>
2018-02-15 16:13:34 +0100

    added forEach for multiple entries, time interval

--- bot.js
+++ bot.js
@@ -17,46 +17,51 @@ const hashtags = fs.readFile('./hashtags.json', "utf8", (err, data) => {
     if (err) {
         console.err(err)
     } else {
-        retweet(data)
+        init(data)
     }
 })
 
 // entering the data
 
- retweet = function(data) {
-    const theData = JSON.parse(data)
-    const theHashTag = theData.hashtags[0].tag
-    const params = {
-        q: theHashTag,
-        result_type: 'recent',
-        lang: 'en'
-    }
+
+
+
+    retweet = function(data) {
+        const theData = JSON.parse(data)
+        theData.hashtags.forEach(item => {
+            const theHashTag = item.tag
+            const params = {
+                q: theHashTag,
+                result_type: 'recent'
+            }
+
 
 // retweet function (inside previous function)
 
-    Twitter.get('search/tweets', params, function(err, data) {
+        Twitter.get('search/tweets', params, function(err, data) {
 
-        // if there no errors
-        if (!err) {
-            // grab ID of tweet to retweet
-            const retweetId = data.statuses[0].id_str;
-            // Tell TWITTER to retweet
-            Twitter.post('statuses/retweet/:id', {
-                id: retweetId
-            }, function(err, response) {
-                if (response) {
-                    console.log('Retweet successful')
-                }
-                // if there was an error while tweeting
-                if (err) {
-                    console.log('Error retweeting, check for duplicates')
-                }
-            });
-        }
-        // if unable to Search a tweet
-        else {
-            console.log('Error searching - try again')
-        }
+            // if there no errors
+            if (!err) {
+                // grab ID of tweet to retweet
+                const retweetId = data.statuses[0].id_str
+                // Tell TWITTER to retweet
+                Twitter.post('statuses/retweet/:id', {
+                    id: retweetId
+                }, function(err, response) {
+                    // if there was an error while tweeting
+                    if (err) {
+                        console.log('Error retweeting, check for duplicates')
+                    }
+                    if (response) {
+                        console.log('Retweet successful')
+                    }
+                });
+            }
+            // if unable to Search a tweet
+            else {
+                console.log('Error searching - try again')
+            }
+        })
     })
 }
 
@@ -69,6 +74,11 @@ app.listen(3000, function() {
 
 // execute
 
-app.get('/', function ( req, res ) {
-    setInterval(retweet, 3000000000000)
+const init = function(data) {
+    setInterval(function(){retweet(data) 
+    }, 60000)
+}
+
+app.get('/', function(req, res)  {
+    console.log('blah')
 })
